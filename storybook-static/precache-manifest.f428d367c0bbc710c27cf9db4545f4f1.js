73359239168f3ce2ac1b070935527ce297e5e88e
Rexrover2 <lsleong@student.unimelb.edu.au>
2020-09-22 14:23:33 +1000

    taskbox UI

---
+++ storybook-static/precache-manifest.f428d367c0bbc710c27cf9db4545f4f1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2394033e195b6f88411a90622b34e65f",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.4db8990db8b53f38b6f0.bundle.js"
+  },
+  {
+    "url": "runtime~main.4db8990db8b53f38b6f0.bundle.js"
+  },
+  {
+    "revision": "ed099ee2fd47d6042814",
+    "url": "static/css/main.67471bcc.chunk.css"
+  },
+  {
+    "url": "vendors~main.4db8990db8b53f38b6f0.bundle.js"
+  },
+  {
+    "url": "vendors~main.4db8990db8b53f38b6f0.bundle.js.LICENSE.txt"
+  }
+]);
